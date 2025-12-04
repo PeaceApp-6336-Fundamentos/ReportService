0e59281ffb635fc871fa6a735c3f8b3480676e72
Bryan Espejo <U202213278@upc.edu.pe>
2025-12-04 18:58:45 -0500

    test: add H2 test dependency, application-test.properties and test profile config

--- src/test/java/com/upc/pre/peaceapp/reports/ReportServiceApplicationTests.java
+++ src/test/java/com/upc/pre/peaceapp/reports/ReportServiceApplicationTests.java
@@ -2,8 +2,11 @@ package com.upc.pre.peaceapp.reports;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class ReportServiceApplicationTests {
 
     @Test
